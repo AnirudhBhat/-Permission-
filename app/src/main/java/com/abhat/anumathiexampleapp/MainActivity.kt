da836ff892956ac93393a69e6d88cf49b97fcbd8
Anirudh Uppunda <abhat38@gmail.com>
2022-04-29 12:49:13 +0530

    Adds an example app which uses Activity to demonstrate the usage of Anumathi library.

--- app/src/main/java/com/abhat/anumathiexampleapp/MainActivity.kt
+++ app/src/main/java/com/abhat/anumathiexampleapp/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.abhat.anumathiexampleapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.abhat.anumathi.PermissionResult
+import com.abhat.anumathi.anumathi
+import com.abhat.anumathi.registerPermissions
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    private val registerPermission = registerPermissions()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        GlobalScope.launch {
+            val result = anumathi(
+                registerPermission,
+                arrayOf(
+                    Manifest.permission.BLUETOOTH_CONNECT,
+                    Manifest.permission.CALL_PHONE,
+                    Manifest.permission.MANAGE_EXTERNAL_STORAGE,
+                    Manifest.permission.ACCESS_MEDIA_LOCATION
+                )
+            )
+            result.map { (permission, state) ->
+                when (state) {
+                    is PermissionResult.Denied -> {
+                        Log.d("ANUMATHI", "Permission Denied for ${permission}: Rationale - " + state.shouldShowRationale)
+                    }
+                    PermissionResult.Granted -> {
+                        Log.d("ANUMATHI", "Permission Granted ${permission}")
+                    }
+                }
+            }
+        }
     }
 }
